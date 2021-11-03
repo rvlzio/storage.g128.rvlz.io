03c446ada798567ab5b8029609be97e0f90f35a5
Ricardo Veloz <ricardo@rvlz.io>
2021-11-03 02:32:50 +0000

    Clean up database connection logic in cli tool
    All of the database connection logic is placed in a decorator for resuse
    across all commands that need it.

---
+++ management-tool/commands/decorators.py
@@ -0,0 +1,32 @@
+import functools
+import os
+
+import click
+import psycopg2
+
+def connection(func):
+    @functools.wraps(func)
+    def connection_wrapper(*args, **kwargs):
+        domain = os.environ["STORAGE_SERVICE_DATABASE_DOMAIN"]
+        user = os.environ["STORAGE_SERVICE_DATABASE_USER"]
+        password = os.environ["STORAGE_SERVICE_DATABASE_PASSWORD"]
+        database_name = kwargs.get(
+            'database_name',
+            os.environ["STORAGE_SERVICE_ADMIN_DATABASE"],
+        )
+        conn = None
+        try:
+            conn = psycopg2.connect(
+                host=domain,
+                dbname=database_name,
+                user=user,
+                password=password,
+            )
+            func(conn, *args, **kwargs)
+        except Exception as err:
+            click.echo(f"Database operation failed.")
+            click.echo(f"exception: \'{str(err).strip()}\'")
+        finally:
+            if conn is not None:
+                conn.close()
+    return connection_wrapper
