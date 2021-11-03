8cddc917b7571e0e236d246eae3efb0bf61f280e
Ricardo Veloz <ricardo@rvlz.io>
2021-11-03 03:52:22 +0000

    Remove 'handler' suffix from cli tool commands
    Change create_database_handler to create_database and
    drop_database_handler to drop_database.

--- management-tool/manager.py
+++ management-tool/manager.py
@@ -10,13 +10,13 @@ def cli():
 
 @cli.command("create-database")
 @click.argument("database_name")
-def create_database_handler(database_name):
+def create_database(database_name):
     database.create_database(database_name)
 
 
 @cli.command("drop-database")
 @click.argument("database_name")
-def drop_database_handler(database_name):
+def drop_database(database_name):
     database.drop_database(database_name)
 
 
